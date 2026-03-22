eddc0541169233a888c2eed3c3fb03bd4d59174a
itsvlxd <itsvladalbu@proton.me>
2026-03-22 13:18:13 +0200

    feat(filemananger): add proper yazi suppor for gtk apps

--- cmds/system/setup/filemanager.sh
+++ cmds/system/setup/filemanager.sh
@@ -55,9 +55,46 @@ setup_file_manager() {
         $helper -S --noconfirm "$target_fm"
     fi
 
+    if [[ $target_fm == "yazi" ]]; then
+        local term_cmd=$(get_var "RETRO_TERMINAL_CMD")
+        [[ -z $term_cmd || $term_cmd == "null" ]] && term_cmd="kitty"
+
+        rx_log "info" "Generating terminal wrapper for Yazi to ensure GUI compatibility."
+        mkdir -p "$HOME/.local/share/applications"
+        cat <<EOF >"$HOME/.local/share/applications/retro-yazi.desktop"
+[Desktop Entry]
+Type=Application
+Name=Yazi (Retro)
+Exec=$term_cmd -e yazi %u
+Icon=yazi
+Terminal=true
+Categories=System;FileManagement;
+MimeType=inode/directory;application/x-directory;
+EOF
+    fi
+
     set_var "RETRO_FILEMANAGER_CMD" "$target_fm"
-    xdg-mime default "$desktop_file" inode/directory
-    xdg-settings set default-url-scheme-handler file "$desktop_file"
+
+    local mime_types=("inode/directory" "application/x-directory" "x-scheme-handler/file" "x-scheme-handler/trash")
+    for type in "${mime_types[@]}"; do
+        xdg-mime default "$desktop_file" "$type"
+    done
+
+    for mime_file in "$HOME/.config/mimeapps.list" "$HOME/.local/share/applications/mimeapps.list"; do
+        if [[ -f $mime_file ]]; then
+            sed -i '/inode\/directory/d; /application\/x-directory/d; /x-scheme-handler\/file/d' "$mime_file"
+            sed -i "/\[Default Applications\]/a inode/directory=$desktop_file\napplication/x-directory=$desktop_file\nx-scheme-handler/file=$desktop_file" "$mime_file"
+        fi
+    done
+
+    [[ -x $(command -v update-desktop-database) ]] && update-desktop-database ~/.local/share/applications &>/dev/null
+
+    if command -v gsettings &>/dev/null; then
+        local term_cmd=$(get_var "RETRO_TERMINAL_CMD")
+        [[ -z $term_cmd || $term_cmd == "null" ]] && term_cmd="kitty"
+        gsettings set org.gnome.desktop.default-applications.terminal exec "$term_cmd"
+        gsettings set org.gnome.desktop.default-applications.terminal exec-arg '-e'
+    fi
 
     rx_log "success" "The system file manager has been standardized to ${PINK}$target_fm${RESET} and saved to the configuration."
 }
