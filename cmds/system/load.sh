f3cea877f5e7936f104b07ae2c2de68357c83b91
itsvlxd <itsvladalbu@proton.me>
2026-03-29 13:46:00 +0300

    fix(power): fix power management rules not applying properly

--- cmds/system/load.sh
+++ cmds/system/load.sh
@@ -8,9 +8,9 @@ cmd_load() {
     local action="$1"
 
     local startup_tasks=(
-        "retro --wallpaper restore|Applying last used wallpaper"
         "retro --power restore|Restoring hardware power profiles"
         "retro --event restart|Initializing event loop and custom hooks"
+        "retro --wallpaper restore|Applying last used wallpaper"
 
         "wl-paste --type text --watch cliphist store -ignore-secrets|Starting cliphist text store watcher"
         "wl-paste --type image --watch cliphist store -ignore-secrets|Starting cliphist image store watcher"
