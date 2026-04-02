b23f5293b4ecada077b13bd7ad0ef457e6c5867f
itsvlxd <itsvladalbu@proton.me>
2026-04-03 01:23:23 +0300

    feat(bluetooth): add bluetooth utility manager and better bluetooth system integration

---
+++ lib/icons.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rx_get_icon() {
+    local name="${1,,}"
+
+    local base_dir="${RETRO_DIR:-.}"
+    local icon_dir="$base_dir/icons"
+    local icon_path=""
+
+    case "$name" in
+        *"xbox"*"wireless"*) icon_path="$icon_dir/xbox_wireless_controller.png" ;;
+        *"nothing"*"headphone"*) icon_path="$icon_dir/nothing_headphone_1.png" ;;
+        *) icon_path="bluetooth" ;;
+    esac
+
+    if [[ $icon_path == *.png ]] && [[ ! -f $icon_path ]]; then
+        echo "bluetooth-active"
+    else
+        echo "$icon_path"
+    fi
+}
