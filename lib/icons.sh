238e772ad5da597971746d570062db0cd11e3122
itsvlxd <itsvladalbu@proton.me>
2026-04-07 23:59:41 +0300

    feat(icons): add icons for AT Over Ear

--- lib/icons.sh
+++ lib/icons.sh
@@ -10,6 +10,7 @@ rx_get_icon() {
     case "$name" in
         *"xbox"*"wireless"*) icon_path="$icon_dir/xbox_wireless_controller.png" ;;
         *"nothing"*"headphone"*) icon_path="$icon_dir/nothing_headphone_1.png" ;;
+        *"at"*"over"*"ear"*) icon_path="$icon_dir/at_over_ear.png" ;;
         *) icon_path="bluetooth" ;;
     esac
 
