221d36d43b0b4c61320b4a8370eaefae16b2fdbd
itsvlxd <itsvladalbu@proton.me>
2026-04-24 14:01:08 +0300

    chore(workflow): change the nightly release readme shields order

--- iso/profile/profiledef.sh
+++ iso/profile/profiledef.sh
@@ -18,6 +18,9 @@ file_permissions=(
   ["/root"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
+  ["/root/configurator"]="0:0:755"
+  ["/root/retrolinux-install/"]="0:0:755"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/var/cache/retrolinux/mirror/offline/"]="0:0:775"
 )
