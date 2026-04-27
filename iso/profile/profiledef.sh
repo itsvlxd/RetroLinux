6571439b4f818ff1d98cbf08dae85d0e55352ed7
itsvlxd <itsvladalbu@proton.me>
2026-04-28 01:14:33 +0300

    fix(iso): fix profiledef build permission error

--- iso/profile/profiledef.sh
+++ iso/profile/profiledef.sh
@@ -23,7 +23,7 @@ file_permissions=(
     ["/opt/retrolinux/"]="0:0:755"
     ["/opt/retrolinux/bin/"]="0:0:755"
     ["/opt/retrolinux/bin/retroinstall"]="0:0:755"
-    ["/opt/retrolinux/bin/helpers/"]="0:0:755"
+    ["/opt/retrolinux/bin/setup/"]="0:0:755"
     ["/opt/retrolinux/bin/lib/"]="0:0:755"
     ["/opt/retrolinux/bin/logo.txt"]="0:0:644"
 )
