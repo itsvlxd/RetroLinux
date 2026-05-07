bc1550c56141f6717c027dc00cbfec68eb06f3dc
itsvlxd <itsvladalbu@proton.me>
2026-05-08 01:25:17 +0300

    chore(bin): simplfy the bin post modules script to use the new install flags

--- bin/post/modules.sh
+++ bin/post/modules.sh
@@ -6,9 +6,6 @@ rx_post_install_modules() {
     rx_clear_logo
     rx_step "Installing RetroLinux modules..."
 
-    local ROOT_MODULES=("grub" "plymouth")
-    local USER_MODULES=("hyprland" "matugen" "kitty" "retro")
-
     local username=$(arch-chroot /mnt getent passwd 1000 2>/dev/null | cut -d: -f1)
 
     if [[ -z $username ]]; then
@@ -24,47 +21,13 @@ rx_post_install_modules() {
         return 1
     fi
 
-    if [[ ${#ROOT_MODULES[@]} -gt 0 ]]; then
-        gum style "Installing root modules..."
-        echo
+    gum style "Installing root modules..."
 
-        for module in "${ROOT_MODULES[@]}"; do
-            local output
-            output=$(arch-chroot /mnt /opt/retrolinux/retro.sh --install "$module" -y 2>&1)
-            local result=$?
+    arch-chroot /mnt /opt/retrolinux/retro.sh -i all -a root -y 2>&1
 
-            local last_line=$(echo "$output" | tail -1)
+    gum style "Installing user modules..."
 
-            if [[ $result -eq 0 ]] && ! echo "$last_line" | grep -iq "error"; then
-                gum style --foreground 2 "$module module installed"
-            else
-                gum style --foreground 3 "Warning: $module module had issues"
-            fi
-        done
-
-        echo
-    fi
-
-    if [[ ${#USER_MODULES[@]} -gt 0 ]]; then
-        gum style "Installing user modules..."
-        echo
-
-        for module in "${USER_MODULES[@]}"; do
-            local output
-            output=$(arch-chroot /mnt su - "$username" -c "/opt/retrolinux/retro.sh --install $module -y" 2>&1)
-            local result=$?
-
-            local last_line=$(echo "$output" | tail -1)
-
-            if [[ $result -eq 0 ]] && ! echo "$last_line" | grep -iq "error"; then
-                gum style --foreground 2 "$module module installed"
-            else
-                gum style --foreground 3 "Warning: $module module had issues"
-            fi
-        done
-
-        echo
-    fi
+    arch-chroot /mnt su - "$username" -c "/opt/retrolinux/retro.sh -i all -a user -y" 2>&1
 
     gum style --foreground 2 "Module installation complete"
     echo
