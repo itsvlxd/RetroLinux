dfdd19e963e1cc21ff8e1b2535f0ef777c12ef87
itsvlxd <itsvladalbu@proton.me>
2026-05-14 13:23:47 +0300

    chore(events): remove unused watcher and events funcctions

--- scripts/watchers/bluetooth.sh
+++ scripts/watchers/bluetooth.sh
@@ -43,32 +43,6 @@ check_bluetooth_state() {
     last_bt_connected_list="$current_list"
 }
 
-_bt_check_incoming() {
-    local obex_dir="$HOME/Downloads"
-    local last_file
-    last_file=$(get_var "BT_LAST_INCOMING_FILE" "")
-
-    if [[ -d $obex_dir ]]; then
-        local newest
-        newest=$(find "$obex_dir" -maxdepth 1 -type f -newer /tmp/.bt_watcher_start 2>/dev/null | sort -r | head -1)
-        if [[ -n $newest && "$newest" != "$last_file" ]]; then
-            local sender_name sender_mac
-            sender_name=$(get_var "BT_INCOMING_SENDER_NAME" "Unknown")
-            sender_mac=$(get_var "BT_INCOMING_SENDER_MAC" "")
-
-            local file_name=$(basename "$newest")
-            local file_size
-            file_size=$(du -h "$newest" 2>/dev/null | awk '{print $1}')
-
-            broadcast_event "on_bluetooth_file_received" "$sender_name" "$sender_mac" "$file_name" "$newest" "$file_size"
-
-            set_var "BT_LAST_INCOMING_FILE" "$newest"
-        fi
-    fi
-
-    touch /tmp/.bt_watcher_start 2>/dev/null
-}
-
 start_watcher_bluetooth() {
     [[ $(has_bluetooth) != "true" ]] && exit 0
 
@@ -76,7 +50,6 @@ start_watcher_bluetooth() {
 
     while true; do
         check_bluetooth_state
-        _bt_check_incoming
         sleep 2
     done
 }
