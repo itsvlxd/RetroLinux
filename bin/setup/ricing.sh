8d03430def79dbb4bac962b8a8df72be78a4b27b
itsvlxd <itsvladalbu@proton.me>
2026-05-19 19:33:46 +0300

    chore(bin): add the post install on first boot notice

--- bin/setup/ricing.sh
+++ bin/setup/ricing.sh
@@ -10,7 +10,7 @@ setup_ricing() {
     gum style --foreground 2 --padding "1 0 0 $PADDING_LEFT" "Stable Mode (Recommended)"
     gum style --foreground 7 --padding "0 0 0 $PADDING_LEFT" "Uses pre-configured files managed via retro CLI and the settings app."
     gum style --foreground 7 --padding "0 0 1 $PADDING_LEFT" "This is the most stable and recommended approach."
-    echo
+
     gum style --foreground 3 --padding "1 0 0 $PADDING_LEFT" "Advanced Mode"
     gum style --foreground 7 --padding "0 0 0 $PADDING_LEFT" "Rewrites all configuration files during installation."
     gum style --foreground 7 --padding "0 0 1 $PADDING_LEFT" "Note: retro CLI and settings app ricing is more stable and recommended."
