51ab049eac5c659c62dfa9569a172dab49b68981
itsvlxd <itsvladalbu@proton.me>
2026-05-21 15:58:41 +0300

    chore(setup): remove redudant xdg setup in variables.sh

--- cmds/system/setup/variables.sh
+++ cmds/system/setup/variables.sh
@@ -2,7 +2,6 @@
 
 source "$RETRO_DIR/lib/log.sh"
 source "$RETRO_DIR/lib/colors.sh"
-source "$RETRO_DIR/lib/xdg.sh"
 
 setup_variables() {
     rx_log "info" "Initializing system variables..."
@@ -23,23 +22,6 @@ setup_variables() {
         "$RETRO_DIR/retro.sh" variable set "$key" "$val" 2>/dev/null
     done
     rx_log "success" "System variables initialized"
-
-    if [[ $filemanager_choice == "yazi" ]]; then
-        mkdir -p "$HOME/.local/share/applications"
-        cat >"$HOME/.local/share/applications/yazi.desktop" <<'EOF'
-[Desktop Entry]
-Type=Application
-Name=Yazi
-Exec=kitty -e yazi %u
-Icon=yazi
-Terminal=true
-Categories=System;FileManagement;
-MimeType=inode/directory;application/x-directory;
-EOF
-    fi
-
-    rx_xdg_reset_defaults >/dev/null
-    rx_log "success" "File manager configured: $filemanager_choice"
 
     command -v gsettings &>/dev/null && gsettings set org.gnome.desktop.default-applications.terminal exec 'kitty'
 }
