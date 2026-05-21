2e3ee43e80cf0139538a4c57b1ad1cb3a7354799
itsvlxd <itsvladalbu@proton.me>
2026-05-22 01:34:52 +0300

    chore(modules): add minimal rx_log logging to the grub module

--- modules/grub/install.sh
+++ modules/grub/install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+source "$RETRO_DIR/lib/log.sh"
 source "$RETRO_DIR/scripts/grub_core.sh"
 
+rx_log "info" "Installing GRUB themes..."
 install_grub_themes
+
+rx_log "info" "Updating GRUB configuration..."
 update_grub_config
+
+rx_log "info" "Regenerating GRUB configuration..."
 regenerate_grub
+
+rx_log "success" "GRUB module installation complete"
