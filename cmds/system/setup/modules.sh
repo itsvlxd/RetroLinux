ba52e6e479d281b288d948155fbd098cdc368b51
itsvlxd <itsvladalbu@proton.me>
2026-05-31 18:22:42 +0300

    feat(setup): add better logs for the modules post install on first boot

--- cmds/system/setup/modules.sh
+++ cmds/system/setup/modules.sh
@@ -6,9 +6,17 @@ source "$RETRO_DIR/lib/colors.sh"
 setup_modules() {
     rx_log "info" "Installing RetroLinux modules..."
 
-    sudo "$RETRO_DIR/retro.sh" -i all -a root -y 2>&1
+    local type_flag=""
+    if [[ $install_type == "minimal" ]]; then
+        type_flag="-t core"
+        rx_log "info" "Installing minimal set (core modules only)..."
+    else
+        type_flag="-t all"
+        rx_log "info" "Installing complete set (all modules)..."
+    fi
 
-    "$RETRO_DIR/retro.sh" -i all -a user -y 2>&1
+    retro -i all -a root $type_flag -y
+    retro -i all -a user $type_flag -y
 
     rx_log "success" "Module installation complete"
 }
