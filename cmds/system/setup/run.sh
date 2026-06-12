87575c040f864b828c99e4ee79911a6add3bc3e4
itsvlxd <itsvladalbu@proton.me>
2026-06-12 12:50:11 +0300

    chore(setup): add better spacing

--- cmds/system/setup/run.sh
+++ cmds/system/setup/run.sh
@@ -43,6 +43,7 @@ run_postinstall() {
     check_dep "mpv" "mpv"
 
     retro xdg setup -o "editor=${EDITOR_CHOICE:-nvim},browser=${BROWSER_CHOICE:-firefox},filemanager=${FILEMANAGER_CHOICE:-nemo},image=loupe,video=mpv"
+
     retro wallpaper setup --needed -o "theme=retro"
     retro theme setup --needed -y
     retro power setup --needed -o "profile=recommended"
