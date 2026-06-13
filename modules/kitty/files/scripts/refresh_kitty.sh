dd1fbe1bfdf1cfc7fbb16aa7ca0b92e4c4330d71
itsvlxd <itsvladalbu@proton.me>
2026-06-13 11:09:06 +0300

    feat(kitty): make kitty font be fully customizable

--- modules/kitty/files/scripts/refresh_kitty.sh
+++ modules/kitty/files/scripts/refresh_kitty.sh
@@ -9,17 +9,23 @@ SIZE=$(get_var "KITTY_FONT_SIZE" "9.5")
 PADDING=$(get_var "KITTY_PADDING" "5")
 
 THEME_FILE="$HOME/.config/retro/themes/kitty-colors.conf"
+FONTS_FILE="$HOME/.config/retro/themes/kitty-fonts.conf"
+
+mkdir -p "$(dirname "$FONTS_FILE")"
+cat >"$FONTS_FILE" <<FONTCONF
+font_family      $FONT
+font_size        $SIZE
+window_padding_width    $PADDING
+symbol_map U+E000-U+F8FF $FONT
+FONTCONF
 
 apply_settings() {
     local target=$1
 
     kitty @ $target set-colors -a "$THEME_FILE"
 
-    kitty @ $target set-font-family "$FONT"
     kitty @ $target set-font-size "$SIZE"
     kitty @ $target set-background-opacity "$OPACITY"
-    kitty @ $target set-font-symbols --ranges U+E000-U+F8FF,U+F0000-U+FFFFF "$FONT"
-    kitty @ $target set-font-symbols --ranges U+1F300-U+1F9FF,U+1F600-U+1F64F "$EMOJI"
 
     kitty @ $target set-spacing \
         padding-top="$PADDING" \
