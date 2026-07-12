057e8e56e5861860845af17fd387eac3ff2032e4
itsvlxd <itsvladalbu@proton.me>
2026-07-13 00:50:39 +0300

    feat(load): add hypridle startup command

--- cmds/system/load.sh
+++ cmds/system/load.sh
@@ -30,8 +30,9 @@ cmd_load() {
 
         "rbw config set sync_interval $(get_var 'CLIP_WARDEN_SYNC')|Synchronizing vault refresh interval with global security policy"
         "rbw config set lock_timeout $(get_var 'CLIP_WARDEN_TIMEOUT')|Enforcing automated vault hibernation and session locking"
-        "hypridle -c ${XDG_CONFIG_HOME}/retro/hypridle.conf|Starting Hyprland idle daemon"
     )
+
+    [[ $(get_var "HYPRIDLE_ENABLE" "true") == "true" ]] && startup_tasks+=("hypridle -c ${XDG_CONFIG_HOME}/retro/hypridle.conf|Starting Hyprland idle daemon")
 
     local custom_tasks=()
     local custom_raw=$(get_var "RETRO_CUSTOM_LOAD")
