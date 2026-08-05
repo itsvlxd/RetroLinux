3b709e7883101e6668ec6be83087d3e6321d5341
itsvlxd <itsvladalbu@proton.me>
2026-08-05 18:36:01 +0300

    feat(retroshell): add tesseract packages

--- modules/retroshell/packages.sh
+++ modules/retroshell/packages.sh
@@ -7,3 +7,8 @@ ttf-phosphor-icons ttf-league-gothic
 unzip curl
 brightnessctl networkmanager
 wl-clipboard slurp hyprpicker upower
+
+tesseract tesseract-data-eng tesseract-data-spa
+tesseract-data-deu tesseract-data-fra tesseract-data-pol
+tesseract-data-ron tesseract-data-swe
+tesseract-data-jpn tesseract-data-chi_sim tesseract-data-chi_tra tesseract-data-kor
