ae20e6343592c00680dfb4a90b37c018a3d64884
itsvlxd <itsvladalbu@proton.me>
2026-08-06 15:51:34 +0300

    feat(retro): add missing default files inside retro

---
+++ modules/retro/files/themes/.cache/colors/da1b8bc14b613e51d0b10d021528ab66/light/.config/retro/themes/hyprland-colors.lua
@@ -0,0 +1,104 @@
+return {
+    image = "/home/vlad/.config/retro/wallpapers/retro/retrowave-gtr-wallpaper.jpg",
+
+    background = "0xfff9f9f9",
+
+    error = "0xffba1a1a",
+
+    error_container = "0xffffdad6",
+
+    inverse_on_surface = "0xfff1f1f1",
+
+    inverse_primary = "0xfff8adfc",
+
+    inverse_surface = "0xff303030",
+
+    on_background = "0xff1b1b1b",
+
+    on_error = "0xffffffff",
+
+    on_error_container = "0xff410002",
+
+    on_primary = "0xffffffff",
+
+    on_primary_container = "0xff36003f",
+
+    on_primary_fixed = "0xff36003f",
+
+    on_primary_fixed_variant = "0xff6b2d72",
+
+    on_secondary = "0xffffffff",
+
+    on_secondary_container = "0xff251626",
+
+    on_secondary_fixed = "0xff251626",
+
+    on_secondary_fixed_variant = "0xff534153",
+
+    on_surface = "0xff1b1b1b",
+
+    on_surface_variant = "0xff474747",
+
+    on_tertiary = "0xffffffff",
+
+    on_tertiary_container = "0xff33110c",
+
+    on_tertiary_fixed = "0xff33110c",
+
+    on_tertiary_fixed_variant = "0xff673b34",
+
+    outline = "0xff777777",
+
+    outline_variant = "0xffc6c6c6",
+
+    primary = "0xff85468c",
+
+    primary_container = "0xffffd6fd",
+
+    primary_fixed = "0xffffd6fd",
+
+    primary_fixed_dim = "0xfff8adfc",
+
+    scrim = "0xff000000",
+
+    secondary = "0xff6c586b",
+
+    secondary_container = "0xfff4dbf1",
+
+    secondary_fixed = "0xfff4dbf1",
+
+    secondary_fixed_dim = "0xffd8bfd5",
+
+    shadow = "0xff000000",
+
+    source_color = "0xffbf49d0",
+
+    surface = "0xfff9f9f9",
+
+    surface_bright = "0xfff9f9f9",
+
+    surface_container = "0xffeeeeee",
+
+    surface_container_high = "0xffe8e8e8",
+
+    surface_container_highest = "0xffe2e2e2",
+
+    surface_container_low = "0xfff3f3f3",
+
+    surface_container_lowest = "0xffffffff",
+
+    surface_dim = "0xffdadada",
+
+    surface_tint = "0xff85468c",
+
+    surface_variant = "0xffe2e2e2",
+
+    tertiary = "0xff82524a",
+
+    tertiary_container = "0xffffdad4",
+
+    tertiary_fixed = "0xffffdad4",
+
+    tertiary_fixed_dim = "0xfff6b8ad",
+
+}
--- modules/retro/files/themes/hyprland-colors.lua
+++ modules/retro/files/themes/hyprland-colors.lua
@@ -1,104 +1,104 @@
 return {
-    image = "/home/vlad/.config/retro/wallpaper_frames/car-in-neon-gas-station.mp4.png",
+    image = "/home/vlad/.config/retro/wallpapers/retro/retrowave-gtr-wallpaper.jpg",
 
-    background = "0xff17111a",
+    background = "0xff131313",
 
     error = "0xffffb4ab",
 
     error_container = "0xff93000a",
 
-    inverse_on_surface = "0xff352e38",
+    inverse_on_surface = "0xff303030",
 
-    inverse_primary = "0xff9300da",
+    inverse_primary = "0xff85468c",
 
-    inverse_surface = "0xffebdeed",
+    inverse_surface = "0xffe2e2e2",
 
-    on_background = "0xffebdeed",
+    on_background = "0xffe2e2e2",
 
     on_error = "0xff690005",
 
     on_error_container = "0xffffdad6",
 
-    on_primary = "0xff4f0077",
+    on_primary = "0xff51145a",
 
-    on_primary_container = "0xfff5d9ff",
+    on_primary_container = "0xffffd6fd",
 
-    on_primary_fixed = "0xff30004b",
+    on_primary_fixed = "0xff36003f",
 
-    on_primary_fixed_variant = "0xff7000a7",
+    on_primary_fixed_variant = "0xff6b2d72",
 
-    on_secondary = "0xff412742",
+    on_secondary = "0xff3b2b3c",
 
-    on_secondary_container = "0xfffdd7fa",
+    on_secondary_container = "0xfff4dbf1",
 
-    on_secondary_fixed = "0xff2a132c",
+    on_secondary_fixed = "0xff251626",
 
-    on_secondary_fixed_variant = "0xff593d5a",
+    on_secondary_fixed_variant = "0xff534153",
 
-    on_surface = "0xffebdeed",
+    on_surface = "0xffe2e2e2",
 
-    on_surface_variant = "0xffcfc3d0",
+    on_surface_variant = "0xffc6c6c6",
 
-    on_tertiary = "0xff4c2141",
+    on_tertiary = "0xff4c251f",
 
-    on_tertiary_container = "0xffffd7ef",
+    on_tertiary_container = "0xffffdad4",
 
-    on_tertiary_fixed = "0xff330b2b",
+    on_tertiary_fixed = "0xff33110c",
 
-    on_tertiary_fixed_variant = "0xff653758",
+    on_tertiary_fixed_variant = "0xff673b34",
 
-    outline = "0xff988d9a",
+    outline = "0xff919191",
 
-    outline_variant = "0xff4c444f",
+    outline_variant = "0xff474747",
 
-    primary = "0xffe5b4ff",
+    primary = "0xfff8adfc",
 
-    primary_container = "0xff7000a7",
+    primary_container = "0xff6b2d72",
 
-    primary_fixed = "0xfff5d9ff",
+    primary_fixed = "0xffffd6fd",
 
-    primary_fixed_dim = "0xffe5b4ff",
+    primary_fixed_dim = "0xfff8adfc",
 
     scrim = "0xff000000",
 
-    secondary = "0xffe0bbde",
+    secondary = "0xffd8bfd5",
 
-    secondary_container = "0xff593d5a",
+    secondary_container = "0xff534153",
 
-    secondary_fixed = "0xfffdd7fa",
+    secondary_fixed = "0xfff4dbf1",
 
-    secondary_fixed_dim = "0xffe0bbde",
+    secondary_fixed_dim = "0xffd8bfd5",
 
     shadow = "0xff000000",
 
-    source_color = "0xffa83be9",
+    source_color = "0xffbf49d0",
 
-    surface = "0xff17111a",
+    surface = "0xff131313",
 
-    surface_bright = "0xff3e3641",
+    surface_bright = "0xff393939",
 
-    surface_container = "0xff241d27",
+    surface_container = "0xff1f1f1f",
 
-    surface_container_high = "0xff2f2732",
+    surface_container_high = "0xff2a2a2a",
 
-    surface_container_highest = "0xff3a323d",
+    surface_container_highest = "0xff353535",
 
-    surface_container_low = "0xff201923",
+    surface_container_low = "0xff1b1b1b",
 
-    surface_container_lowest = "0xff120c15",
+    surface_container_lowest = "0xff0e0e0e",
 
-    surface_dim = "0xff17111a",
+    surface_dim = "0xff131313",
 
-    surface_tint = "0xffe5b4ff",
+    surface_tint = "0xfff8adfc",
 
-    surface_variant = "0xff4c444f",
+    surface_variant = "0xff474747",
 
-    tertiary = "0xfff1b4dc",
+    tertiary = "0xfff6b8ad",
 
-    tertiary_container = "0xff653758",
+    tertiary_container = "0xff673b34",
 
-    tertiary_fixed = "0xffffd7ef",
+    tertiary_fixed = "0xffffdad4",
 
-    tertiary_fixed_dim = "0xfff1b4dc",
+    tertiary_fixed_dim = "0xfff6b8ad",
 
 }
