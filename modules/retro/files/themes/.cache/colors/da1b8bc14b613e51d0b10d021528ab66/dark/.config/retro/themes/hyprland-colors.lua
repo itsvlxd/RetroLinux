d0e6bf0a4b8b8680bcf1b2ba675153707fa55d2b
itsvlxd <itsvladalbu@proton.me>
2026-08-06 15:51:34 +0300

    feat(retro): add missing default files inside retro

---
+++ modules/retro/files/themes/.cache/colors/da1b8bc14b613e51d0b10d021528ab66/dark/.config/retro/themes/hyprland-colors.lua
@@ -0,0 +1,104 @@
+return {
+    image = "/home/vlad/.config/retro/wallpapers/retro/retrowave-gtr-wallpaper.jpg",
+
+    background = "0xff131313",
+
+    error = "0xffffb4ab",
+
+    error_container = "0xff93000a",
+
+    inverse_on_surface = "0xff303030",
+
+    inverse_primary = "0xff85468c",
+
+    inverse_surface = "0xffe2e2e2",
+
+    on_background = "0xffe2e2e2",
+
+    on_error = "0xff690005",
+
+    on_error_container = "0xffffdad6",
+
+    on_primary = "0xff51145a",
+
+    on_primary_container = "0xffffd6fd",
+
+    on_primary_fixed = "0xff36003f",
+
+    on_primary_fixed_variant = "0xff6b2d72",
+
+    on_secondary = "0xff3b2b3c",
+
+    on_secondary_container = "0xfff4dbf1",
+
+    on_secondary_fixed = "0xff251626",
+
+    on_secondary_fixed_variant = "0xff534153",
+
+    on_surface = "0xffe2e2e2",
+
+    on_surface_variant = "0xffc6c6c6",
+
+    on_tertiary = "0xff4c251f",
+
+    on_tertiary_container = "0xffffdad4",
+
+    on_tertiary_fixed = "0xff33110c",
+
+    on_tertiary_fixed_variant = "0xff673b34",
+
+    outline = "0xff919191",
+
+    outline_variant = "0xff474747",
+
+    primary = "0xfff8adfc",
+
+    primary_container = "0xff6b2d72",
+
+    primary_fixed = "0xffffd6fd",
+
+    primary_fixed_dim = "0xfff8adfc",
+
+    scrim = "0xff000000",
+
+    secondary = "0xffd8bfd5",
+
+    secondary_container = "0xff534153",
+
+    secondary_fixed = "0xfff4dbf1",
+
+    secondary_fixed_dim = "0xffd8bfd5",
+
+    shadow = "0xff000000",
+
+    source_color = "0xffbf49d0",
+
+    surface = "0xff131313",
+
+    surface_bright = "0xff393939",
+
+    surface_container = "0xff1f1f1f",
+
+    surface_container_high = "0xff2a2a2a",
+
+    surface_container_highest = "0xff353535",
+
+    surface_container_low = "0xff1b1b1b",
+
+    surface_container_lowest = "0xff0e0e0e",
+
+    surface_dim = "0xff131313",
+
+    surface_tint = "0xfff8adfc",
+
+    surface_variant = "0xff474747",
+
+    tertiary = "0xfff6b8ad",
+
+    tertiary_container = "0xff673b34",
+
+    tertiary_fixed = "0xffffdad4",
+
+    tertiary_fixed_dim = "0xfff6b8ad",
+
+}
