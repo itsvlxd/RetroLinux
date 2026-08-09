08de5a9af05ad2bde6aa1ed5b93ad6be4f0e8f62
itsvlxd <itsvladalbu@proton.me>
2026-08-09 13:17:24 +0300

    feat(bin): make the qr code smaller

--- bin/lib/qr.sh
+++ bin/lib/qr.sh
@@ -10,6 +10,15 @@ rx_gather_system_info() {
         info+="OS: RetroLinux (unknown version)\n"
     fi
 
+    if [[ -f /sys/class/dmi/id/product_name ]]; then
+        local product_name=$(cat /sys/class/dmi/id/product_name 2>/dev/null)
+        [[ -n $product_name ]] && info+="Product: $product_name\n"
+    fi
+
+    if command -v uname &>/dev/null; then
+        info+="Kernel: $(uname -r)\n"
+    fi
+
     if command -v lscpu &>/dev/null; then
         local cpu_model=$(lscpu | grep "Model name" | cut -d: -f2 | xargs)
         [[ -z $cpu_model ]] && cpu_model=$(lscpu | grep "Model:" | cut -d: -f2 | xargs)
@@ -28,20 +37,63 @@ rx_gather_system_info() {
         [[ -n $storage ]] && info+="Storage: $storage\n"
     fi
 
+    if command -v lspci &>/dev/null; then
+        local pci_count=$(lspci 2>/dev/null | wc -l)
+        info+="PCI Devices: ${pci_count}\n"
+
+        local gpu_model=$(lspci 2>/dev/null | grep -iE "vga|3d|display" | head -n1 | sed 's/^[0-9a-f:.]* //; s/^[A-Za-z0-9 ]*controller: //; s/^[A-Za-z0-9 ]*display controller: //')
+        [[ -n $gpu_model ]] && info+="GPU: $gpu_model\n"
+    fi
+
+    if command -v lsusb &>/dev/null; then
+        local usb_count=$(lsusb 2>/dev/null | wc -l)
+        info+="USB Devices: ${usb_count}\n"
+
+        if lsusb 2>/dev/null | grep -qi "bluetooth"; then
+            info+="Bluetooth: Yes\n"
+        else
+            info+="Bluetooth: No\n"
+        fi
+    fi
+
+    if [[ -d /sys/class/dmi/id ]]; then
+        local board_vendor=$(cat /sys/class/dmi/id/board_vendor 2>/dev/null)
+        local board_name=$(cat /sys/class/dmi/id/board_name 2>/dev/null)
+        if [[ -n $board_vendor && -n $board_name ]]; then
+            info+="Motherboard: ${board_vendor} ${board_name}\n"
+        fi
+
+        local bios_vendor=$(cat /sys/class/dmi/id/bios_vendor 2>/dev/null)
+        local bios_version=$(cat /sys/class/dmi/id/bios_version 2>/dev/null)
+        if [[ -n $bios_vendor && -n $bios_version ]]; then
+            info+="BIOS: ${bios_vendor} ${bios_version}\n"
+        fi
+    fi
+
+    if [[ -d /sys/firmware/efi ]]; then
+        info+="Firmware: UEFI\n"
+    else
+        info+="Firmware: BIOS\n"
+    fi
+
     local wifi_driver wifi_card eth_driver eth_card
 
-    wifi_card=$(ip link show 2>/dev/null | grep -E "^[0-9]+: wlan[0-9]+" | awk -F': ' '{print $2}' | head -n1)
+    wifi_card=$(rx_get_wifi_iface)
     if [[ -n $wifi_card ]]; then
         wifi_driver=$(ethtool -i "$wifi_card" 2>/dev/null | grep driver | cut -d: -f2 | xargs)
         [[ -z $wifi_driver ]] && wifi_driver="unknown"
         info+="WiFi: $wifi_card (driver: $wifi_driver)\n"
+    else
+        info+="WiFi: none\n"
     fi
 
-    eth_card=$(ip link show 2>/dev/null | grep -E "^[0-9]+: (eth|en)" | awk -F': ' '{print $2}' | head -n1)
+    eth_card=$(rx_get_ethernet_iface)
     if [[ -n $eth_card ]]; then
         eth_driver=$(ethtool -i "$eth_card" 2>/dev/null | grep driver | cut -d: -f2 | xargs)
         [[ -z $eth_driver ]] && eth_driver="unknown"
         info+="Ethernet: $eth_card (driver: $eth_driver)\n"
+    else
+        info+="Ethernet: none\n"
     fi
 
     echo -e "$info"
@@ -108,12 +160,12 @@ Please describe what happened below:"
     local encoded_title encoded_body full_url
     encoded_title=$(rx_encode_url "$issue_title")
 
-    local body_max=600
+    local body_max=800
     encoded_body=$(rx_encode_url "${issue_body:0:body_max}")
     full_url="https://${repo_url}?title=${encoded_title}&body=${encoded_body}"
 
     if [[ ${#full_url} -gt 2800 ]]; then
-        encoded_body=$(rx_encode_url "${issue_body:0:400}")
+        encoded_body=$(rx_encode_url "${issue_body:0:500}")
         full_url="https://${repo_url}?title=${encoded_title}&body=${encoded_body}"
     fi
 
