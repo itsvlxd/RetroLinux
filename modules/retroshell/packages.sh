264a7d55215822bed0403c38481c02077629ccd5
itsvlxd <itsvladalbu@proton.me>
2026-08-18 12:27:06 +0300

    chore(retroshell): remove tesseract deps

--- modules/retroshell/packages.sh
+++ modules/retroshell/packages.sh
@@ -9,8 +9,3 @@ brightnessctl networkmanager syntax-highlighting
 wl-clipboard slurp hyprpicker upower gpu-screen-recorder
 
 go
-
-tesseract tesseract-data-eng tesseract-data-spa
-tesseract-data-deu tesseract-data-fra tesseract-data-pol
-tesseract-data-ron tesseract-data-swe
-tesseract-data-jpn tesseract-data-chi_sim tesseract-data-chi_tra tesseract-data-kor
