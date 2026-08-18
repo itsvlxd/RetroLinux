0598778fc5b7416259bc49bb0e0277588604257d
itsvlxd <itsvladalbu@proton.me>
2026-08-18 12:50:36 +0300

    feat(modules): add tesseract ocr

---
+++ modules/tesseract/packages.sh
@@ -0,0 +1,4 @@
+tesseract tesseract-data-eng tesseract-data-spa
+tesseract-data-deu tesseract-data-fra tesseract-data-pol
+tesseract-data-ron tesseract-data-swe
+tesseract-data-jpn tesseract-data-chi_sim tesseract-data-chi_tra tesseract-data-kor
