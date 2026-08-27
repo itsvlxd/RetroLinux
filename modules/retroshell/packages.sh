88d6adb6bcc2053ab72d043e981b3dbef139eb73
itsvlxd <itsvladalbu@proton.me>
2026-08-27 23:52:26 +0300

    chore(retroshell): remove libinput packages

--- modules/retroshell/packages.sh
+++ modules/retroshell/packages.sh
@@ -1,14 +1,13 @@
 quickshell
 qt6-base qt6-declarative qt6-wayland qt6-svg
+
 qt6-imageformats qt6-multimedia qt6-shadertools
 
 ttf-phosphor-icons ttf-league-gothic
 
-libinput-tools
 unzip curl
 brightnessctl networkmanager syntax-highlighting
 wl-clipboard slurp hyprpicker upower gpu-screen-recorder
 pulseaudio-utils mpg123
-libinput-tools evtest
 
 go
